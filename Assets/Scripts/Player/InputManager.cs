eaf4caaa36ea5339dce4eecfa0f36795b6bfc5c5
Ian-PC\Ian <funnyguy150@gmail.com>
2018-11-20 16:54:52 -0500

    Menus, start of player controller

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public enum PlayerNumber { P1, P2, P3, P4, PC }
+    public enum ButtonName {Horizontal,Vertical, Fire1 , Fire2, Fire3 , Fire4, Rtrigger,Ltrigger }
+    public PlayerNumber playerNumber;
+    public Player player;
+    public void Awake()
+    {
+        //player = ReInput.players.GetPlayer((int)playerNumber);
+    }
+    public float GetAxis( string axisName)
+    {
+
+        return player.GetAxis(axisName);
+
+
+    }
+ 
+    public bool GetButton( ButtonName buttonName)
+    {
+      
+            return player.GetButton(buttonName.ToString());
+
+    }
+
+    public bool GetButtonDown( ButtonName buttonName)
+    {
+
+            return player.GetButtonDown(buttonName.ToString());
+
+  
+    }
+   
+    public  bool GetButtonUp(ButtonName buttonName)
+    {
+    
+            return player.GetButtonUp(buttonName.ToString());
+
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerPlatformerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject {
+
+	public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    AudioSource audioData;
+
+	private SpriteRenderer spriteRenderer;
+
+	void awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		audioData = GetComponent<AudioSource>();
+
+	}
+
+	protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+             velocity.y = jumpTakeOffSpeed; 
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+		if (Input.GetButtonDown("Fire1"))
+        {
+            //shoot();
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+		targetVelocity = move * maxSpeed;
+    }
+
+	/*void shoot()
+    {
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        //audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+
+        bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.forward * 6;
+        Destroy(bullet, 2.0f);
+	}*/
+}
